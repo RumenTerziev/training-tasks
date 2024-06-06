c3477465456cff1e5143d53b826a05bcf85f1a4c
Rumen Terziev <106109989+RumenTerziev@users.noreply.github.com>
2024-06-06 15:24:50 +0300

    Added a training task.

---
+++ src/main/java/org/example/hacker/SaveThePrisoner.java
@@ -0,0 +1,47 @@
+package org.example.hacker;
+
+import java.io.*;
+import java.util.stream.IntStream;
+
+public class SaveThePrisoner {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int m = Integer.parseInt(firstMultipleInput[1]);
+
+                int s = Integer.parseInt(firstMultipleInput[2]);
+
+                int result = saveThePrisoner(n, m, s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    public static int saveThePrisoner(int countChairs, int countSweets, int startingChair) {
+        int result;
+        int module = countSweets % countChairs;
+        if (module == 0) {
+            result = startingChair - 1;
+            return result == 0 ? countChairs : result;
+        }
+        result = module + startingChair - 1;
+        return result > countChairs ? result % countChairs : result;
+    }
+}
