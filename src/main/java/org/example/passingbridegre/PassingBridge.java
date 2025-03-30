bab11b0e4bbc7b539298ca0a6f1f832ed6e02ba7
Rumen Terziev <106109989+RumenTerziev@users.noreply.github.com>
2025-03-30 15:25:22 +0300

    Added comments.

--- src/main/java/org/example/passingbridegre/PassingBridge.java
+++ src/main/java/org/example/passingbridegre/PassingBridge.java
@@ -17,6 +17,7 @@ public class PassingBridge {
         List<Double> friendsOnLeftSide = new ArrayList<>(times);
         List<Double> friendsOnRightSide = new ArrayList<>();
 
+        // Get the passing time of the fastest and the second fastest that will pass the bridge for the first time
         double first = friendsOnLeftSide.stream()
                 .min(Double::compare)
                 .orElse(0.0);
@@ -37,28 +38,32 @@ public class PassingBridge {
                 .max(Double::compare)
                 .orElse(0.0);
 
+        // The two fastest pass the bridge
         friendsOnRightSide.add(first);
         friendsOnRightSide.add(second);
 
         totalTime += second;
 
+        // The fastest one goes back with the lantern
         friendsOnRightSide.remove(first);
-
         friendsOnLeftSide.add(first);
         totalTime += first;
 
+        // The slowest to pass the bridge
         friendsOnLeftSide.remove(third);
         friendsOnLeftSide.remove(fourth);
         friendsOnRightSide.add(third);
         friendsOnRightSide.add(fourth);
         totalTime += fourth;
 
+        // Second-fastest goes bask
         friendsOnRightSide.remove(second);
         totalTime += second;
 
         friendsOnLeftSide.remove(first);
         friendsOnLeftSide.remove(second);
 
+        // The two fastest pass the bridge again
         friendsOnRightSide.add(first);
         friendsOnRightSide.add(second);
         totalTime += second;
